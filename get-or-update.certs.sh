e1efc2ab41b0fe934e52f68b67f01437cf6ebeb6
Alekasndr Demidov <demidoff@1vp.ru>
2026-08-11 11:23:28 +0000

    chore: improve script with better logging and error handling
    -

--- get-or-update.certs.sh
+++ get-or-update.certs.sh
@@ -5,17 +5,14 @@ WEBROOT="/var/www/certbot"
 
 # Create webroot directory if it doesn't exist
 sudo mkdir -p "$WEBROOT"
-sudo chown -R www-data:www-data $WEBROOT  # Adjust user/group as needed
-sudo chmod -R 755 $WEBROOT
+sudo chown -R www-data:www-data "$WEBROOT"  # Adjust user/group as needed for your system
+sudo chmod -R 755 "$WEBROOT"
 
 # Get all NGINX config files
-CONFIG_FILES=$(find /etc/nginx/sites-available/ -name "*.conf" | grep -v "\.conf~")
+CONFIG_FILES=$(find /etc/nginx/sites-available/ -name "*.odoo.conf" | grep -v "\.conf~")
 
 # Get the directory of the current script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-# echo $SCRIPT_DIR
-# exit 0
 
 # Iterate over each config file
 for config_file in $CONFIG_FILES; do
@@ -24,18 +21,33 @@ for config_file in $CONFIG_FILES; do
 
     # Request certificates for all domains in the config file
     for domain in $DOMAINS; do
-        echo $domain
-        if sudo certbot certonly --webroot -w $WEBROOT -d $domain --non-interactive --agree-tos -m demidoff@1vp.ru; then
-            # Certificate request successful, create symlink
-            sudo ln -sf "$config_file" "/etc/nginx/sites-enabled/$(basename "$config_file")"
+        # Check if a certificate already exists for the domain
+        if sudo certbot certificates | grep -q "$domain"; then
+            echo "Certificate already exists for $domain"
         else
-            # Certificate request failed, remove symlink if it exists
-            if [ -L "/etc/nginx/sites-enabled/$(basename "$config_file")" ]; then
-                sudo rm "/etc/nginx/sites-enabled/$(basename "$config_file")"
+            echo "Requesting certificate for $domain"
+            if sudo certbot certonly --webroot -w "$WEBROOT" -d "$domain" --non-interactive --agree-tos -m demidoff@1vp.ru; then
+                # Certificate request successful, create symlink
+                echo "Certificate request successful for $domain"
+                sudo ln -sf "$config_file" "/etc/nginx/sites-enabled/$(basename "$config_file")"
+            else
+                # Certificate request failed, remove symlink if it exists
+                echo "Certificate request failed for $domain"
+                if [ -L "/etc/nginx/sites-enabled/$(basename "$config_file")" ]; then
+                    sudo rm "/etc/nginx/sites-enabled/$(basename "$config_file")"
+                fi
             fi
         fi
     done
 done
 
 # Reload NGINX
-sudo nginx -t && sudo systemctl reload nginx
+echo "Reloading NGINX"
+if sudo nginx -t && sudo systemctl reload nginx; then
+    echo "NGINX reloaded successfully"
+else
+    echo "NGINX reload failed"
+    exit 1
+fi
+
+exit 0
